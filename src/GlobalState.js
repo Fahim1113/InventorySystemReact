de3aa69a236dee0d0b5cf5b9a8d44cbf9c3ad049
Fahim1113 <fahimshahriyarc@gmail.com>
2022-11-02 21:48:31 +0000

    q

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -3,6 +3,6 @@ import { createGlobalState } from "react-hooks-global-state";
 const { getGlobalState, setGlobalState } = createGlobalState({
   username: "",
   password: "",
-  url: "https://inventory-system-by-fahim.herokuapp.com/",
+  url: "https://inventory-system-by-fahim.herokuapp.com",
 });
 export { getGlobalState, setGlobalState };
